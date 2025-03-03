2bac287d7b939a7ae0246ae63f222e6a2bc53260
ghost <ghostpy91.com>
2025-03-03 10:26:21 -0800

    layout setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+  const loggedIn = { firstName: "Admin  ", lastName: 'XML', email:'admin@gmail.com' };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.45}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
 
-export default Home
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 127.36},{currentBalance: 417.12}]} />
+    </section>
+  );
+};
+
+export default Home;
